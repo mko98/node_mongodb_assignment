681cdd77f874144ade72fc6b511e5c96df74e0b0
Justin Kannekens <jpj.kannekens@student.avans.nl>
2017-11-24 09:24:03 +0100

    Model aangepast, post half werkend. 2.0

---
+++ model/ingredient.model.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const IngredientSchema = new Schema({
+    name: String,
+    amount: Number
+});
+
+module.exports = IngredientSchema;
